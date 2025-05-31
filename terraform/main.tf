6e5c635e27d9a28ece36a0e21158c65d81578c2b
jaydipd <jay.android14@gmail.com>
2025-06-01 02:56:14 +0530

    added code to pick availibility zone manually

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,14 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0] # picks the first available AZ
   tags = {
     Name = "employee-public-subnet"
   }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 # Internet Gateway
