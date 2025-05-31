47464b38b8ecfbf53c1cf5527216c0be78a082cf
jaydipd <jay.android14@gmail.com>
2025-06-01 01:58:29 +0530

    Checking git-jenkins trigger

--- src/main/java/com/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/employee_management/EmployeeManagementApplication.java
@@ -8,10 +8,9 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
-@EnableDynamoDBRepositories(basePackages = "com.employee_management.repository",
-		includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Repository.class))
+@EnableDynamoDBRepositories(basePackages = "com.employee_management.repository", includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
 public class EmployeeManagementApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
 }
